55377343e988fe433f680f028f40b3e9db015881
Derek <derek.mears@outlook.com>
2022-08-17 06:38:38 -0400

    updated readme.md

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -1,4 +1,36 @@
+# loading necessary libraries
 library(tidyverse)
+
+#importing the mpg data set
 mecha_mpg <- read.csv("MechaCar_mpg.csv")
+#running linear regression model agains the mpg data set, and summarizing results
 model_mpg <- lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD,mecha_mpg)
 model_summary <- summary(model_mpg)
+
+#importing the suspension coil dataset
+
+susp_coils <- read.csv("Suspension_Coil.csv")
+
+psi_all_lots <- susp_coils %>%
+  summarize(Mean_PSI=mean(PSI), Median_PSI=median(PSI),PSI_Variance=var(PSI),
+PSI_Standard_Deviation=sd(PSI))
+
+psi_by_lot <- susp_coils %>% group_by(Manufacturing_Lot) %>%
+  summarize(Mean_PSI=mean(PSI), Median_PSI=median(PSI),PSI_Variance=var(PSI),
+PSI_Standard_Deviation=sd(PSI), .keep='groups')
+
+
+#https://nathancarter.github.io/how2data/site/how-to-do-a-hypothesis-test-for-population-variance-in-r/
+# for lot3, is the variance statistically significant?
+#var can't exceed 100
+# hyp.var <- 100
+#calc degrees of freedom, number of obs - 1
+# df <- length(lot3_coils)
+# df <- nrow(lot3_coils)
+# df <- nrow(lot3_coils)-1
+#calculate ratio of vars, out test stat
+# test.statistic <- df*var(lot2_coils$PSI)/hyp.var
+# test.statistic <- df*var(lot1_coils$PSI)/hyp.var
+# test.statistic <- df*var(lot3_coils$PSI)/hyp.var
+#use chi sq test to determine significance
+# 2*pchisq(test.statistic,df=df,lower.tail = FALSE)
--- README.md
+++ README.md
@@ -1,1 +1,32 @@
 # MechaCar_Statistical_Analysis
+
+## Linear Regression to Predict MPG
+
+In our analysis of variables that can be used to predict fuel efficiency (in mpg),
+the following two variables were found to be statistically significant:
+- vehicle_length
+- ground_clearance
+
+The slope of the regression line was also found to be statistically significant.
+Given the statistical significance of the intercept, we can say with confidence
+that it's not equal to zero.
+
+With an adjusted R-squared of 0.6825, we can conclude that the model does have
+reasonable predictive power for the mpg of MechaCar prototypes.
+
+## Summary Statistics on Suspension Coils
+
+In the analysis of 3 suspension coil manufacturing lots it was determined that one
+of the lots, Lot3, does not meet the design specification.
+The design spec requires that the variance of coil PSI can't exceed 100 PSI.
+As can be seen below, at the aggregate level (all lots combined) the combinded
+lots do meet the requirement,but when analyzed by lot, as noted, Lot3 falls outside the
+desired tolerance.
+
+### Overall Summary:
+
+ ![Overall Summary]('total_summary.png')
+
+### Lot Summary:
+
+![Lot Summary]('lot_summary.png')
